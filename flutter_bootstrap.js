2ad4bdc0af1655ba00a11250cb2072e59ff776e1
Melbert Buligan <melbertbuligan777@gmail.com>
2025-05-25 11:13:54 +0800

    Updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3890582990"
+    serviceWorkerVersion: "1535025753"
   }
 });
